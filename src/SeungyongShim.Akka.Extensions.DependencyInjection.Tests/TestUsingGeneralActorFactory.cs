c3805e45e6fd6352d67c5c4133d461e019f07e6a
심승용 <seungyong.shim@mirero.co.kr>
2020-11-09 13:55:36 +0900

    Scrutor 의존성 추가

--- src/SeungyongShim.Akka.Extensions.DependencyInjection.Tests/TestUsingGeneralActorFactory.cs
+++ src/SeungyongShim.Akka.Extensions.DependencyInjection.Tests/TestUsingGeneralActorFactory.cs
@@ -1,30 +1,35 @@
 namespace Tests
 {
+    using System;
+    using System.Reflection;
     using System.Threading.Tasks;
     using Akka.Actor;
     using Akka.DI.Core;
+    using Akka.DI.Extensions.DependencyInjection.TestKit;
     using Akka.TestKit.Xunit2;
     using FluentAssertions;
     using FluentAssertions.Extensions;
     using Microsoft.Extensions.DependencyInjection;
-    using Akka.DI.Extensions.DependencyInjection.TestKit;
     using Xunit;
 
     public class TestUsingGeneralActorFactory : TestKit
     {
+        public TestUsingGeneralActorFactory()
+        {
+           
+        }
+
         [Fact]
         public async Task Check_Child_Actor_Recieved_Messages()
         {
             var services = new ServiceCollection();
             services.AddSingleton<IActorRef>(sp => TestActor);
             services.AddSingleton<IPropsFactory<ChildActor>, PropsFactory<ChildActor, MockChildActor>>();
-            services.AddTransient<ParentActor>();
-            services.AddTransient<MockChildActor>();
             services.AddAkka(Sys);
 
             Sys.UseDependencyInjectionServiceProvider(services.BuildServiceProvider());
 
-            ActorOf(Sys.DI().PropsFactory<ParentActor>().Create(), "Parent");
+            var parent = ActorOf(Sys.DI().PropsFactory<ParentActor>().Create(), "Parent");
 
             ExpectMsg<string>().Should().Be("Hello, Kid");
             ExpectMsg<string>().Should().Be("Hello, Kid");
@@ -57,15 +62,7 @@ namespace Tests
             child2.Path.Name.Should().Be("Child2");
         }
 
-        private class ChildActor : ReceiveActor
-        {
-            public ChildActor()
-            {
-                Receive<string>(_ => { });
-            }
-        }
-
-        private class ParentActor : ReceiveActor
+        public class ParentActor : ReceiveActor
         {
             public ParentActor()
             {
@@ -74,6 +71,14 @@ namespace Tests
 
                 childActor1.Tell("Hello, Kid");
                 childActor2.Tell("Hello, Kid");
+            }
+        }
+
+        private class ChildActor : ReceiveActor
+        {
+            public ChildActor()
+            {
+                Receive<string>(_ => { });
             }
         }
     }
--- src/SeungyongShim.Akka.Extensions.DependencyInjection/UseAkkaExtensions.cs
+++ src/SeungyongShim.Akka.Extensions.DependencyInjection/UseAkkaExtensions.cs
@@ -1,22 +1,81 @@
 namespace Microsoft.Extensions.DependencyInjection
 {
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Reflection;
     using Akka.Actor;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+    using System;
 
     public static class UseAkkaExtensions
     {
-        public static IServiceCollection AddAkka(this IServiceCollection services, ActorSystem actorSystem)
+        
+        private static IEnumerable<Assembly> GetAssemblies(IEnumerable<string> regexFilters)
+        {
+            return from path in new[]
+                   {
+                       Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName),
+                       Directory.GetCurrentDirectory()
+                   }
+                   from ext in new[] { "*.dll", "*.exe" }
+                   from file in Directory.GetFiles(path, ext)
+                   let assembly = TryLoadFrom(file)
+                   where assembly != null
+                   where !Regex.IsMatch(assembly.FullName, "^Microsoft.*")
+                   where !Regex.IsMatch(assembly.FullName, "^Akka.*")
+                   where !Regex.IsMatch(assembly.FullName, "^System.*")
+                   where !Regex.IsMatch(assembly.FullName, "^xunit.*")
+                   where regexFilters.Where(x => Regex.IsMatch(assembly.GetName().Name, x))
+                                     .Any()
+                   select assembly;
+
+            Assembly TryLoadFrom(string assemblyFile)
+            {
+                try
+                {
+                    return Assembly.LoadFrom(assemblyFile);
+                }
+                catch (Exception ex)
+                {
+                    return null;
+                }
+            }
+        }
+
+        public static IServiceCollection AddAkka(this IServiceCollection services, ActorSystem actorSystem, IEnumerable<string> autoRegistrationTargetAssemblies = null)
         {
             services.AddSingleton<ActorSystem>(sp => actorSystem);
             services.AddSingleton(typeof(IPropsFactory<>), typeof(PropsFactory<>));
 
+            var assemblies = GetAssemblies(new[]
+            {
+                $"^{Assembly.GetExecutingAssembly().GetName().Name}$",
+                $"^{Assembly.GetCallingAssembly().GetName().Name}$",
+            }
+            .Concat(autoRegistrationTargetAssemblies?? new[] {""}) )
+            .ToList();
+
+            services.Scan(sc =>
+            {
+                sc.FromAssemblies(assemblies)
+                  .AddClasses(classes => classes.AssignableTo<ReceiveActor>())
+                  .AsSelf()
+                  .WithTransientLifetime();
+            });
+
             return services;
         }
 
-        public static IServiceCollection AddAkka(this IServiceCollection services, string actorSystemName, Akka.Configuration.Config actorSystemConfig = null)
+        public static IServiceCollection AddAkka(this IServiceCollection services,
+                                                 string actorSystemName,
+                                                 Akka.Configuration.Config actorSystemConfig = null,
+                                                 IEnumerable<string> autoRegistrationTargetAssemblies = null)
         {
             var actorSystem = ActorSystem.Create(actorSystemName, actorSystemConfig);
 
-            return services.AddAkka(actorSystem);
+            return services.AddAkka(actorSystem, autoRegistrationTargetAssemblies);
         }
     }
 }
